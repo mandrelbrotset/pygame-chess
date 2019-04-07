20570bbaf0ea51da998e9d885f80deb795c7b990
Sheriff Olaoye <sheriffolaoye@gmail.com>
2019-04-07 00:20:42 -0500

    Imported resources, created chess and piece class

---
+++ chess.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Chess:
+    def __init__(self):
+        x = ""
+
+    def menu(self):
+        x = None
+
+    def game(self):
+        x = None
+
+if __name__=="__main__":
+    Chess()
